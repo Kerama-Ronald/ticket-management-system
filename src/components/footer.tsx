e27b3741d25d2f2af91b84171f83a35afb3c01af
Ronald Kerama <keramaronald11@gmail.com>
2023-08-31 21:37:06 +0300

    Adoption of responsive navbar

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -23,9 +23,9 @@ function Footer() {
 				</div>
 				<div className="p-5">
 					<ul>
-						<p className="text-gray-800 font-bold text-2xl pb-4">Product</p>
+						<p className="text-gray-800 font-bold text-2xl pb-4">Risk Analysis</p>
 						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Stocks
+							FAQs
 						</li>
 						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Futures & Options
@@ -50,9 +50,7 @@ function Footer() {
 						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Pricing
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Careers
-						</li>
+						
 						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Press & Media
 						</li>
@@ -73,9 +71,7 @@ function Footer() {
 						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
 							Downloads & Resources
 						</li>
-						<li className="text-gray-500 text-md pb-2 font-semibold hover:text-blue-600 cursor-pointer">
-							Videos
-						</li>
+						
 					</ul>
 				</div>
 			</div>
---
+++ src/components/home.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './BluePage.css'; // You can create this CSS file to style your page
+
+interface TeamMemberProps {
+  name: string;
+  role: string;
+  imageSrc: string;
+}
+
+const TeamMember: React.FC<TeamMemberProps> = ({ name, role, imageSrc }) => (
+  <div className="team-member">
+    <div className="member-image">
+      <img src={imageSrc} alt={name} />
+    </div>
+    <div className="member-details">
+      <h3>{name}</h3>
+      <p>{role}</p>
+    </div>
+  </div>
+);
+
+const BluePage: React.FC = () => (
+  <div className="blue-page">
+    <h1>MEET OUR IT TEAM</h1>
+    <div className="team-container">
+      <TeamMember
+        name="Brian Tovo"
+        role="IT Associate"
+        imageSrc="brian-tovo-image-url.jpg" // Replace with actual image URL
+      />
+      <TeamMember
+        name="Tandon Samora"
+        role="SOC Analyst"
+        imageSrc="tandon-samora-image-url.jpg" // Replace with actual image URL
+      />
+    </div>
+  </div>
+);
+
+export default BluePage;
